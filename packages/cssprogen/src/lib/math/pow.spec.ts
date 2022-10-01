bbfdab85325af0cc80a1c9b9298a9328582641a0
Alessandro Maclaine <almaclaine@gmail.com>
2022-09-30 23:30:56 -0500

    add modularPow

--- packages/cssprogen/src/lib/math/pow.spec.ts
+++ packages/cssprogen/src/lib/math/pow.spec.ts
@@ -1,4 +1,5 @@
-import { pow } from './pow';
+import { modularPow, pow } from './pow';
+import { modularRatios } from '../general/get-modular-ratio';
 
 describe('pow works', () => {
   it('pow works work', () => {
@@ -6,5 +7,10 @@ describe('pow works', () => {
     expect(pow(5, 0)).toBe(1);
     expect(pow(2, 2)).toBe(4);
     expect(pow(5)).toBe(25);
+  });
+
+  it('modularPow works work', () => {
+    expect(modularPow(modularRatios.unison, 2)).toBe(1);
+    expect(modularPow(modularRatios.doubleOctave, 2)).toBe(16);
   });
 });
--- packages/cssprogen/src/lib/math/pow.ts
+++ packages/cssprogen/src/lib/math/pow.ts
@@ -1,2 +1,7 @@
+import { ModularScaleRatio } from '../types/modular-scale-ratio';
+import { getModularRatio } from '../general/get-modular-ratio';
+
 const pow = (base: number, power = 2) => Math.pow(base, power);
-export { pow };
+const modularPow = (ratio: ModularScaleRatio, power = 2) =>
+  Math.pow(getModularRatio(ratio), power);
+export { pow, modularPow };
